ad9b8f187f83e0b08a2d295ddc6c5cce04a33f6b
Emmanuel Lusinchi <emmanuel.lusinchi@gmail.com>
2015-01-21 23:37:36 +0800

    Added comments
    Commented the body of both makeCacheMatrix and cacheSolve functions.

--- cachematrix.R
+++ cachematrix.R
@@ -5,17 +5,27 @@
 ## This function creates a special "matrix" object
 ## that can cache its inverse.
 ## 
-## Return a list containing the matrix and set,get,setSolve,getSolve functions
+## Return a list containing set,get,setSolve,getSolve functions
 makeCacheMatrix <- function(x = matrix()) {
 
+  # The solved matrix will be saved in s
+  # If not solved yet, s will be NULL
   s <- NULL
+  
+  # provide set and get functions for the matrix itself
   set <- function(y) {
     x <<- y
     s <<- NULL
+    # Note the use of <<- to assign x and s in another environment than their current environment
   }
   get <- function() x
+  
+  # Provide set and get function for the solved matrix. 
   setSolve <- function(solve) s <<- solve
+  # Note the use of <<- to assign s in another environment than its current environment
   getSolve <- function() s
+  
+  # return a list containing the set, get for the matrix and for the cached solved matrix
   list(set = set, get = get,
        setSolve = setSolve,
        getSolve = getSolve)  
@@ -35,15 +45,23 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Return a matrix that is the inverse of 'x'
 cacheSolve <- function(x, ...) {
         
-  
+  # read the cached solved matrix
   s <- x$getSolve()
+  
+  # if it isn't null, it has already been solved. Excellent! Return the cached value.
   if(!is.null(s)) {
     message("getting cached data")
     return(s)
   }
+  
+  # Aw.... we don't have a cached solved matrix, so let's compute it...
   data <- x$get()
   s <- solve(data, ...)
+  
+  # and let's not forget to record the solved matrix in the cache so it can be read the next time.
   x$setSolve(s)
+  
+  # and return the solved matrix
   s
   
 }
